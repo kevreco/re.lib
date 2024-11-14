720793e660d976d10eefd1dd084524f2d881567f
kevreco <kevin.rc@hotmail.fr>
2024-11-14 14:20:51 +0100

    darr_map.h: Update comments and license.

--- darr_map.h
+++ darr_map.h
@@ -1,24 +1,50 @@
-/* darr_map.h - v0.0 - kevreco - CC0 1.0 Licence (public domain) */
-
-/* This library requires darr.h */
-
 /*
 
-NOTES:
-======
+SUMMARY:
 
--
+    Map using sorted arrays.
+    This library requires darr.h
+    
+    See end of file for license information.
+    
+EXAMPLE:
 
-CHANGES (DD/MM/YYYY):
-=====================
+    #include "darr.h"
+    #include "darr_map.h"
+    
+    struct item {
+        int key;
+        int value;
+    };
+    
+    static int item_less(const struct item* left, const struct item* right)
+    {
+        return (left->key < right->key);
+    }
+    
+    int main() {
 
--
-
-TODO:
-=====
-
--
-
+        darr_map map;
+        darr_map_init(&map, sizeof(item), (darr_predicate_t)map_item_less);
+        
+        struct item item = { 0, 1 };
+        
+        // Add item.
+        darr_map_set(&map, &item);
+        
+        // Check item.
+        darr_map_contains(&map, &item);
+        
+        // Remove item.
+        darr_map_remove(&map, &item);
+        
+        darr_map_destroy(&map);
+    }
+    
+    #define DARR_IMPLEMENTATION
+    #include "darr.h"
+    #define DARR_MAP_IMPLEMENTATION
+    #include "darr_map.h"
 */
 
 #ifndef RE_DARR_MAP_H
@@ -134,3 +160,58 @@ darr_map_size(const darr_map* m)
 }
 
 #endif /* RE_DARR_MAP_IMPLEMENTATION */
+
+/*
+------------------------------------------------------------------------------
+This software is available under 2 licenses -- choose whichever you prefer.
+------------------------------------------------------------------------------
+ALTERNATIVE 1 - The MIT License (MIT)
+
+Copyright (c) 2024 kevreco
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+------------------------------------------------------------------------------
+ALTERNATIVE 2 - Public Domain (www.unlicense.org)
+
+This is free and unencumbered software released into the public domain.
+
+Anyone is free to copy, modify, publish, use, compile, sell, or
+distribute this software, either in source code form or as a compiled
+binary, for any purpose, commercial or non-commercial, and by any
+means.
+
+In jurisdictions that recognize copyright laws, the author or authors
+of this software dedicate any and all copyright interest in the
+software to the public domain. We make this dedication for the benefit
+of the public at large and to the detriment of our heirs and
+successors. We intend this dedication to be an overt act of
+relinquishment in perpetuity of all present and future rights to this
+software under copyright law.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+IN NO EVENT SHALL THE AUTHORS BE LIABLE FOR ANY CLAIM, DAMAGES OR
+OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE,
+ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
+OTHER DEALINGS IN THE SOFTWARE.
+
+For more information, please refer to <https://unlicense.org>
+------------------------------------------------------------------------------
+*/
